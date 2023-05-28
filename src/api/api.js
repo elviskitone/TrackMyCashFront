4dd39d26e4dcd186c321c61f6ad877841345dd40
Elvis Peter Kitone <elviskitone@gmail.com>
2023-05-28 10:23:23 +0300

    Corrected the production environment variable

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Set the API endpoint URL
 let API_URL;
 
-if (process.env.NODE_ENV === "producion") {
+if (process.env.NODE_ENV === "production") {
   API_URL = process.env.REACT_APP_API_URL_RENDER;
 } else { 
       API_URL = "http://localhost:3500/api";
